08abd9eb98a6916f3723bc86cfba10e8545e1813
LordinsX <52792476+LordinsX@users.noreply.github.com>
2026-06-16 17:47:21 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyAZRbc37dj2vaiACjAsD0mXUfyne_Wd9gw",
+	authDomain: "lordinsxtest.firebaseapp.com",
+	databaseURL: "https://lordinsxtest-default-rtdb.europe-west1.firebasedatabase.app",
+	projectId: "lordinsxtest",
+	storageBucket: "lordinsxtest.firebasestorage.app",
+	messagingSenderId: "611910355892",
+	appId: "1:611910355892:web:c6ecbd24579c8ee95800e1"
+};
